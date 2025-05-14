c91027d4cc31298f67843bffaefe92a7af875c91
marukun712 <marukun530@gmail.com>
2025-05-14 10:34:57 +0900

    ⚡️ OPFSに保存してみる

--- backend/index.ts
+++ backend/index.ts
@@ -1,52 +1,10 @@
-import { createRxServer } from "rxdb-server/plugins/server";
-import { RxServerAdapterExpress } from "rxdb-server/plugins/adapter-express";
-import { createRxDatabase } from "rxdb";
-import { getRxStorageMongoDB } from "rxdb/plugins/storage-mongodb";
+import { Hono } from "hono";
 
-const db = await createRxDatabase({
-  name: "sns",
-  storage: getRxStorageMongoDB({
-    connection: process.env.DATABASE_URL ?? "",
-  }),
+const app = new Hono();
+app.get("/post", (c) => {
+  const json = c.req.parseBody();
+
+  return c.body("");
 });
 
-const collections = await db.addCollections({
-  posts: {
-    schema: {
-      version: 0,
-      primaryKey: "id",
-      type: "object",
-      properties: {
-        id: {
-          type: "string",
-          maxLength: 64,
-        },
-        text: {
-          type: "string",
-        },
-        publicKey: { type: "string" },
-        timestamp: {
-          type: "string",
-        },
-        signature: {
-          type: "string",
-        },
-      },
-      required: ["id", "text", "publicKey", "timestamp", "signature"],
-    },
-  },
-});
-
-const server = await createRxServer({
-  database: db,
-  adapter: RxServerAdapterExpress,
-  port: 8000,
-  cors: "*",
-});
-
-server.addReplicationEndpoint({
-  name: "sync",
-  collection: collections.posts,
-});
-
-await server.start();
+export default app;
